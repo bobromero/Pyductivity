1963375a5d5f04059f867ace2dbe509577c60ac3
bobromero <rromero123102@gmail.com>
2025-03-19 13:49:10 -0400

    added the main menu and weekly meny

--- src/main.py
+++ src/main.py
@@ -1,11 +1,39 @@
+import weekly
 
-
-prompt = f'''
+# motivationa_quote = 
+weekly_reminder_list = weekly.display_weekly(0)
+main_prompt = f'''
     Welcome to Pyductivity!
-    {motivational_quote}
+    motivational_quote
     This week, remember to focus on
     {weekly_reminder_list}
     A. Today's notes
     B. Todo Lists
     C. Weekly Reminders    
+    Enter to exit
     '''
+
+
+def main_path():
+    choice = input(main_prompt)
+
+    if choice == 'A' or choice == 'a':
+        pass
+
+    elif choice == 'B' or choice == 'b':
+        pass
+
+    elif choice == 'C' or choice == 'c':
+        weekly.weekly_path()
+    elif choice == "":
+        return -1
+    else:
+        print("invalid input: try again")
+    return 0
+                
+
+val = 0 
+while val >= 0: 
+    val = main_path()
+
+
--- src/weekly.py
+++ src/weekly.py
@@ -1,7 +1,14 @@
 from file_modifier import file_reader, file_writer 
 
 weekly_file = "../data/weekly.pk"
-# data_type = 
+
+weekly_prompt = f'''
+    A. Add an item to the weekly list 
+    B. Remove an item from the weekly list
+    C. List the items in the weekly list
+    Enter to go back
+    '''
+
 
 
 def load_weekly():
@@ -9,6 +16,64 @@ def load_weekly():
 
 def save_weekly(data):
     file_writer(weekly_file, data)
+
+def display_weekly(index):
+    data = []
+    try:
+        data = load_weekly()[index]
+    except:
+        print(f"couldn't load weekly at {index}")
+
+    string = data[1]
+    if len(data) < 1:
+        return ""
+    if len(data) <= index:
+        return string 
+    return string 
+
+def add_to_weekly(data, info):
+    data.append([len(data), info])
+    
+def remove_from_weekly(data,index):
+    data.pop(index)
+
+def weekly_path():
+    value = ""
+    weekly_choice = " "
+    weekly_data = load_weekly()
+    while weekly_choice != "":
+        weekly_choice = input(weekly_prompt)
+        if weekly_choice == "A" or weekly_choice == "a":
+            value = input("Enter an item to add on the list\n")
+            if len(value) <= 0:
+                print("nothing added")
+            else:
+                add_to_weekly(weekly_data, value)
+                print("added", value)
+        if weekly_choice == "B" or weekly_choice == "b":
+            print(*weekly_data)
+            try:
+                value = int(input("Enter the index of the item you want to remove\n"))
+                if type(value) == type(0):
+                    remove_from_weekly(weekly_data,value)
+                    print("removed ",value, " from the list\n")
+            except TypeError:
+                print("couldn't process index, not deleting")
+            except:
+                print("Error, could not delete at, ", value)
+
+        if weekly_choice == "C" or weekly_choice == "c":
+            print(*weekly_data)
+
+        if weekly_choice == "D" or weekly_choice == "d":
+            try:
+                print("saving...")
+                save_weekly(weekly_data)
+                print("saving succeded")
+                return
+            except:
+                print("an error occured while saving")
+
 
 
 if __name__ == '__main__':
@@ -19,10 +84,11 @@ if __name__ == '__main__':
     except:
         print(f"Error: could not load data from {weekly_file}")
         print("Creating a new list")
-    info = input("enter a weekly reminder\n")
-    data.append([len(data),info])
     print(data)
-
-    save_weekly(data)
+    # info = input("enter a weekly reminder\n")
+    # data.append([len(data),info])
+    # print(data)
+    # save_weekly(data)
+    
 
     
