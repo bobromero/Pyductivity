8cc27d2fb6a517282eb190c26d28efd9e84477bd
bobromero <rromero123102@gmail.com>
2025-03-27 13:30:44 -0400

    todo

---
+++ src/todo.py
@@ -0,0 +1,6 @@
+# saved list of strings that track todo items based on the topic of the file
+
+
+#so a folder of files that are named after the topic of the todo list
+#each file is a list of strings that are the todo items
+
